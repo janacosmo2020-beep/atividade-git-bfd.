8893dd558806cfcace2cc081e9be944374053846
janacosmo2020-bipe <janacosmo2020@gmail.com>
2025-09-21 12:40:36 -0300

    estrutura_repeticao

---
+++ 5 - estrutura_repeticao.py
@@ -0,0 +1,12 @@
+# estrutura_repeticao.py
+# Exemplo de laços de repetição em Python
+
+# Usando for
+for i in range(1, 8):
+    print("Número:", i)
+
+# Usando while
+contador = 1
+while contador <= 7:
+    print("Contador:", contador)
+    contador += 1
