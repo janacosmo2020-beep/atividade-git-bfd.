630e75b7e3d2d4318193e1bfd36897b02261a86e
janacosmo2020-bipe <janacosmo2020@gmail.com>
2025-09-21 12:37:14 -0300

    controle_condicional

---
+++ 4 - controle_condicional.py
@@ -0,0 +1,11 @@
+# controle_condicional.py
+# Exemplo de estruturas condicionais em Python
+
+idade = 16
+
+if idade < 18:
+    print("Menor de idade")
+elif idade == 18:
+    print("Acabou de atingir a maioridade")
+else:
+    print("Maior de idade")
