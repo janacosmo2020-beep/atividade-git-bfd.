7277512c2c34f9a1938c2555832065e920e6d58b
janacosmo2020-bipe <janacosmo2020@gmail.com>
2025-09-21 12:49:44 -0300

    lista_dict

---
+++ 7 - lista_dict.py
@@ -0,0 +1,17 @@
+# lista_dict.py
+# Exemplo de listas e dicionários em Python
+
+# Lista
+frutas = ["laranja", "uva", "maça"]
+frutas.append("banana")
+print("Lista de frutas:", frutas)
+
+# Dicionário
+aluno = {
+    "nome": "Janaina",
+    "idade": 30,
+    "curso": "Python"
+}
+print("Nome do aluno:", aluno["nome"])
+print("Idade:", aluno["idade"])
+print("Curso:", aluno["curso"])
