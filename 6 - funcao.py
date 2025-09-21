fa5ecf1609e7fc7ede70bcddbc2e0dc0bae3c719
janacosmo2020-bipe <janacosmo2020@gmail.com>
2025-09-21 12:43:42 -0300

    funcao

---
+++ 6 - funcao.py
@@ -0,0 +1,8 @@
+# funcao.py
+# Exemplo de função em Python
+
+def saudacao(nome):
+    return f"Olá, {nome}! Seja bem-vindo ao curso de Python!!"
+
+print(saudacao("Eliana"))
+print(saudacao("Rafael"))
