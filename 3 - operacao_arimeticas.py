4fafa49eac06044eab6a011f804ec8210cd08bb2
janacosmo2020-bipe <janacosmo2020@gmail.com>
2025-09-21 12:30:36 -0300

    operacao_aritmeticas

---
+++ 3 - operacao_arimeticas.py
@@ -0,0 +1,13 @@
+# operacao_arimeticas.py
+# Operações aritméticas básicas em Python
+
+a = 11
+b = 3
+
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+print("Divisão inteira:", a // b)
+print("Módulo (resto):", a % b)
+print("Potência:", a ** b)
